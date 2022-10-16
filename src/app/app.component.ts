b02ea40459f2ca3e83e5fc88a79465c0224fc258
bl00p1ng <blooping@protonmail.com>
2022-10-16 16:31:34 -0500

    Componente para productos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Product } from './models/product.model'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +10,32 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   imgUrl = '';
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'El mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg'
+    },
+    {
+      id: '2',
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      id: '3',
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      id: '4',
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    }
+  ]
 
   // Métodos
   onLoaded(img: string) {
