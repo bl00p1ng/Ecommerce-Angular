1ff059ce63b67aaaaadff1fe676bc25e54387c7a
bl00p1ng <blooping@protonmail.com>
2022-10-17 15:18:19 -0500

    Sidebar, fuente

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
+
+export class NavComponent {
+
+  activeSideMenu: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // Móstrar/ocultar side menu (mobile)
+  toggleSideMenu() {
+    this.activeSideMenu = !this.activeSideMenu;
   }
 
 }
