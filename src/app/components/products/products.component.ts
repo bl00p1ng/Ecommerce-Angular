134c9598fcea0916937a814e0c9374ab1de77ae7
bl00p1ng <blooping@protonmail.com>
2022-10-17 19:21:05 -0500

    Obtener productos desde la API

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Product } from '../../models/product.model';
 import { StoreService } from '../../services/store.service'
+import { ProductsService } from "../../services/products.service";
 
 @Component({
   selector: 'app-products',
@@ -9,44 +10,28 @@ import { StoreService } from '../../services/store.service'
   styleUrls: ['./products.component.scss']
 })
 
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
 
   // Productos agregados al carrito
   shoppingCart: Product[] = [];
   // Total del carrito
   total: number = 0;
 
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'El mejor juguete',
-      price: 565,
-      image: './assets/images/toy.jpg'
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/images/bike.jpg'
-    },
-    {
-      id: '3',
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: './assets/images/album.jpg'
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/books.jpg'
-    }
-  ]
+  products: Product[] = []
 
   constructor(
-    private storeService: StoreService
+    private storeService: StoreService,
+    private productsService: ProductsService
   ) {
     this.shoppingCart = this.storeService.getShoppingCart();
+  }
+
+  ngOnInit(): void {
+    // Obtener productos desde la API
+    this.productsService.getProducts()
+    .subscribe(data => {
+      this.products = data;
+    });
   }
 
   // Recibir el producto agregado al carrito
