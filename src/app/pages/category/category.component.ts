313897cad9a11d0a86b8ff86bec28e970b1a7ab2
bl00p1ng <blooping@protonmail.com>
2022-11-01 17:29:19 -0500

    Pagina de categorias

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/product.model';
+
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-category',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  // ID de la catergoria, se ingresa por la URL
+  categoryId: string | null = null;
+  // Paginación de las consultas a la API
+  limit: number = 6;
+  offset: number = 0;
+  // Almacena los productos traidos de la API
+  products: Product[] = []
+
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      // Obtener el ID de la URL
+      this.categoryId = params.get('id');
+
+      // Obtener los productos asociados al Id de la categoria
+      if (this.categoryId) {
+        this.productsService.getByCategory(this.categoryId, 6, 0)
+        .subscribe(response => this.products = response)
+      }
+    });
+  }
+
+  // Cargar más productos de la API
+  onLoadMore() {
+    if (this.categoryId) {
+      this.offset += this.limit;
+
+      this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
+      .subscribe(data => {
+        this.products = this.products.concat(data);
+      });
+    }
   }
 
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -12,7 +12,7 @@ import { checkTime } from '../interceptors/time.interceptor';
 
 export class ProductsService {
 
-  private apiUrl: string = 'https://young-sands-07814.herokuapp.com/api/products';
+  private apiUrl: string = 'https://young-sands-07814.herokuapp.com/api';
 
   constructor(
     private http: HttpClient
@@ -27,7 +27,7 @@ export class ProductsService {
       params = params.set('offset', offset);
     }
 
-    return this.http.get<Product[]>(this.apiUrl, { params, context: checkTime() })
+    return this.http.get<Product[]>(`${this.apiUrl}/products`, { params, context: checkTime() })
     .pipe(
       map(products => products.map(item => {
         return {
@@ -40,7 +40,7 @@ export class ProductsService {
 
   // Obtener un producto por su ID desde la API
   getProduct(id: string) {
-    return this.http.get<Product>(`${this.apiUrl}/${id}`)
+    return this.http.get<Product>(`${this.apiUrl}/products/${id}`)
     .pipe(
       // Manejar errores en la consulta a la API
       catchError((err: HttpErrorResponse) => {
@@ -60,18 +60,32 @@ export class ProductsService {
     )
   }
 
+  // Obtener todos los productos asociados al ID de una categoria
+  getByCategory(categoryId: string, limit?: number, offset?: number) {
+    let params = new HttpParams();
+
+    if (limit !== undefined && offset !== undefined) {
+      params = params.set('limit', limit);
+      params = params.set('offset', offset);
+    }
+
+    return this.http.get<Product[]>(`${this.apiUrl}/categories/${categoryId}/products`, { params })
+
+
+  }
+
   // Crear un producto en la API
   create(productData: CreateProductDTO) {
-    return this.http.post<Product>(this.apiUrl, productData);
+    return this.http.post<Product>(`${this.apiUrl}/products`, productData);
   }
 
   // Actualizar un producto en la API por su ID
   update(id: string, productData: UpdateProductDTO) {
-    return this.http.put<Product>(`${this.apiUrl}/${id}`, productData);
+    return this.http.put<Product>(`${this.apiUrl}/products/${id}`, productData);
   }
 
   // ELiminar un producto de la API por su ID
   delete(id: string) {
-    return this.http.delete<boolean>(`${this.apiUrl}/${id}`);
+    return this.http.delete<boolean>(`${this.apiUrl}/products/${id}`);
   }
 }
