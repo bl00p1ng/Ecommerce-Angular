9a32305c988de2830f9f0ea2214b41c1056dd104
bl00p1ng <blooping@protonmail.com>
2022-11-01 22:00:05 -0500

    RouterLink y RouterActive

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/product.model';
+import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,15 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 
-export class NavComponent {
+export class NavComponent implements OnInit {
 
   activeSideMenu: boolean = false;
+  // Almacenar las categorias traidas desde la API
+  categories: Category[] = [];
 
-  constructor() { }
+  constructor(
+    private categoriesService: CategoriesService
+  ) { }
+
+  ngOnInit(): void {
+    this.getAllCategories()
+  }
 
   // Móstrar/ocultar side menu (mobile)
   toggleSideMenu() {
     this.activeSideMenu = !this.activeSideMenu;
   }
 
+  // Obtener las categorias
+  getAllCategories() {
+    this.categoriesService.getCategories()
+    .subscribe(response => this.categories = response);
+  }
 }
