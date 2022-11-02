93a04c11647d6d768742205c1b77864001db6143
bl00p1ng <blooping@protonmail.com>
2022-11-01 19:10:44 -0500

    Evitando doble subscribe

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product.model';
+import { switchMap } from 'rxjs/operators';
 
 import { ProductsService } from 'src/app/services/products.service';
 
@@ -14,7 +15,7 @@ export class CategoryComponent implements OnInit {
   // ID de la catergoria, se ingresa por la URL
   categoryId: string | null = null;
   // Paginación de las consultas a la API
-  limit: number = 6;
+  limit: number = 10;
   offset: number = 0;
   // Almacena los productos traidos de la API
   products: Product[] = []
@@ -25,15 +26,23 @@ export class CategoryComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      // Obtener el ID de la URL
-      this.categoryId = params.get('id');
+    this.route.paramMap
+    .pipe(
+      switchMap(params => {
+        // Obtener el ID de la URL
+        this.categoryId = params.get('id');
 
-      // Obtener los productos asociados al Id de la categoria
-      if (this.categoryId) {
-        this.productsService.getByCategory(this.categoryId, 6, 0)
-        .subscribe(response => this.products = response)
-      }
+        // Obtener los productos asociados al Id de la categoria
+        if (this.categoryId) {
+          return this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
+        }
+
+        // Si no hay un ID, no se muestra ningún producto
+        return [];
+      })
+    )
+    .subscribe(data => {
+      this.products = data;
     });
   }
 
