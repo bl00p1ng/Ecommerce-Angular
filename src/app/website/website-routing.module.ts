dc3840eda259c5eed69831e50b658784cdbcb6a3
Andrés López <blooping@protonmail.com>
2022-11-06 20:16:33 -0500

    Modularizando Categories

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -26,8 +26,9 @@ const routes: Routes = [
         component: HomeComponent
       },
       {
-        path: 'category/:id',
-        component: CategoryComponent
+        path: 'category',
+        loadChildren: () => import('./pages/category/category.module')
+        .then(m => m.CategoryModule)
       },
       {
         path: 'product/:id',
--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
-import { CategoryComponent } from './pages/category/category.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -21,7 +20,6 @@ import { SharedModule } from '../shared/shared.module';
   declarations: [
     NavComponent,
     HomeComponent,
-    CategoryComponent,
     CartComponent,
     LoginComponent,
     RegisterComponent,
